7a5ecf355ac3ea5d3c18c92fea345533095235ac
phantie <43893037+phantie@users.noreply.github.com>
2021-01-06 09:16:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ from os.path import join, dirname
 setup(
     author = 'phantie',
     name = 'rcache',
-    version = '0.1',
+    version = '0.2',
     packages = find_packages(),
     long_description = open(join(dirname(__file__), 'README.md')).read(),
     install_requires=[
-            'bmap @ https://github.com/phantie/bound-sized-hash-map/archive/0.4.tar.gz',
+            'bmap @ https://github.com/phantie/bound-sized-hash-map/archive/1.0.tar.gz',
         ]
 )
